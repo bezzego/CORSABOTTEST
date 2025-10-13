fe1d708d26be8e465221ff82491f8c30dbe5cc88
Egor Bezborodov <egor.bezborodoff@gmail.com>
2025-10-13 09:47:31 +0300

    убрал функцию "keys_control_task"

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,6 @@ from src.handlers.user_handlers import user_routers
 from src.handlers.admin_handlers import admin_routers
 from src.logs import getLogger
 from src.config import settings
-from src.services.keys import keys_control_task
 from src.services.scheduler import start_scheduler
 
 aiogram_logger = getLogger("aiogram.event")
@@ -21,7 +20,6 @@ async def main():
     bot = Bot(token=settings.telegram.token)
     await bot.delete_webhook(drop_pending_updates=True)
     await init_database()
-    asyncio.create_task(keys_control_task(bot))
     await start_scheduler(bot)
     dp.include_routers(*user_routers, *admin_routers)
 
